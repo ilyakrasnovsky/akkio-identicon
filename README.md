50e9df0043b53753de98275db36a5d3bc9e7359a
ilyakrasnovsky <ilyakrasnovsky@gmail.com>
2023-09-19 19:44:18 -0400

    done

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # akkio-identicon
 
-* Take
+![Identicons!](img/all.png)
+
+This is [Ilya Krasnovsky's](https://portfolio.ilyak.mywire.org/) submission for the Akkio take-home [challenge](https://akkio.notion.site/Akkio-Take-home-Problem-3d8044df4eea4a5a85edaba31088924a). 
+
+## Objectives and Design
+
+* With this design, legibility of the generated identicons should be feasible at arbitrary scales without too much additional effort. This is because the design uses a well-defined color-placement pattern (with mirroring) that could be numerically scaled in the implementation.  
+* Identicons generated for different names (Ex. `"John", "Jane"`) should be different. We have a decently high likelihood of that by basing the color pattern on an MD5 hash of the names.  
+* Identicons should be aesthetically pleasing (to a reasonable degree given the time constraints). By mirroring the the color pattern in the top left quadrant of the identicon (which is ultimately the "close-to-unique" identifier of the name) to the other three, we get a simple, symmetric image. 
+
+## Setup
+
+### Prerequisites
+
+* Python 3.11 (lower could work, untested)
+* `pipenv`
+
+### Setup
+
+```bash
+pipenv install -d
+pipenv run python main.py
+```
--- main.py
+++ main.py
@@ -1,42 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import hashlib
-from string import ascii_lowercase, digits
-
-def plt_test() -> None:
-  polygon = np.array([
-    [0.0, 0.0],
-    [0.0, 2.0],
-    [1.0, 3.0],
-    [1.0, 0.0]
-  ])
-  plt.plot(polygon[:, 0], polygon[:, 1], "o")
-  plt.axis("off")
-  # ax = plt.gca()
-  # ax.get_xaxis().set_visible(False)
-  # ax.get_yaxis().set_visible(False)
-  plt.show()
-
-def pixel_plot_test() -> None:
-  # data = np.arange(0, 64).reshape(8, 8) 
-  data = np.zeros((8,8))
-  for i in range(0, 32):
-    data[i // 8][i % 8] = i
-  for i in range(63, 32, -1):
-    data[i // 8][i % 8] = 64 -i
-  data[5][5] = None
-  data[7][7] = None
-  identicon = plt.imshow(data, cmap='twilight_shifted', interpolation='nearest', origin='upper')
-  plt.axis("off")
-  plt.show()
-
-def regular_hash() -> None:
-  a = hash("John")
-  b = hash("Jon")
-  print (a)
-  print (b)
+import os
 
 def name_to_pixel_grid(name: str) -> np.ndarray:
+  """
+  Core logic to take in an arbitrary string (conceptually someone's username), and return an 8x8
+  grid of integers, where the integer in each cell is representative of a color for that cell, if the
+  grid were to be considered as a pixel image. It roughly works as follows:
+
+  1. Produce an MD5 hexdigest of the string. This is a 128-bit number represented as 32 four-bit hex values  
+  2. Store the base-10 equivalent of each hex value into a cell in the top-right (4x4) quadrant in row-major
+     order. Store None if the hex value 16 places to the right of the one being considered is even (see comment
+     below for more discussion).
+  3. Mirror the 4x4 quadrant into the other three quadrants 
+  """
   hashed_name = hashlib.md5(name.encode(encoding='utf-8')).hexdigest()
   ret = np.zeros((8,8))
   for i in range(16):
@@ -44,8 +22,12 @@ def name_to_pixel_grid(name: str) -> np.ndarray:
     d1 = int(hashed_name[i], base=16)
     d2 = int(hashed_name[i + 16], base=16)
 
-    # color in d1 (after conversion to (i,j) via row-major order) only if d2 (shifted 16 indices) is even
-    # this condenses the information
+    # color in d1 (after conversion to (i,j) via row-major order) only if d2 (shifted 16 indices) is even.
+    # This condenses the information encoded in the hexdigest from 32 values to 16. This is helpful because
+    # 16 is a perfect square, which means filling in grid values with it in row-major order will produce a 
+    # square (not a rectangle), which is more aesthetically pleasing.
+    # There could be other interesting ways of combining d1 and d2 to do the condensing,
+    # ex. taking an average.
     val = d1 if d2 % 2 == 0 else None
 
     # source (top left quadrant)
@@ -59,25 +41,31 @@ def name_to_pixel_grid(name: str) -> np.ndarray:
 
   return ret
 
-def show_identicon(pixel_grid: np.ndarray) -> None:
-  _ = plt.imshow(pixel_grid, cmap='Blues', interpolation='nearest', origin='upper')
-  # plt.axis("off")  
-  ax = plt.gca()
-  ax.get_xaxis().set_visible(False)
-  ax.get_yaxis().set_visible(False)
-  plt.show()
+def generate_identicons(names: list[str] =["John", "Jane", "931D387731bBbC988B31220", "Ilya"]) -> None:
+  """
+  Given a list of names to convert to identicons, output a <name>.png for each one in the tmp/ folder of
+  this git repository, along with an all.png including all on one canvas. 
+  """
+  outpath = os.path.join(os.path.dirname(__file__), "tmp")
+  for name in names:
+    plt.imshow(name_to_pixel_grid(name), cmap='Blues', interpolation='nearest', origin='upper')
+    ax = plt.gca()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.savefig(os.path.join(outpath, f"{name}.png"))
 
-def md5_hash() -> None:
-  # print(int("A1", 16))
-  # 1baad9235021178dc28a5948f83e6cce
-  print(hashlib.md5("John1".encode()).hexdigest())
-  # print(hashlib.md5(b"John1").digest())
-  # print(hashlib.md5(b"Jon").hexdigest())
+  fig, subs = plt.subplots(len(names), 1)
+  fig.tight_layout()
+  for i, sub in enumerate(subs):
+    sub.imshow(name_to_pixel_grid(names[i]), cmap='Blues', interpolation='nearest', origin='upper')
+    sub.set_title(names[i])
+    sub.get_xaxis().set_visible(False)
+    sub.get_yaxis().set_visible(False)
+  plt.savefig(os.path.join(outpath, "all.png"))
 
 if __name__ == "__main__":
-  show_identicon(pixel_grid=name_to_pixel_grid(name="e28rnc288 c3"))
-  # plt_test()
-  # regular_hash()
-  # hexdigest produces a 128 bit value. There will be 32 hex numbers (4 bits each)
-  # 
-  # pixel_plot_test()
+  """
+  Entry point.
+  """
+  generate_identicons()
+
